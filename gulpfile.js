6fa91de02492543c0dbffb153b9306dd0facb401
Lkabeer <ahmed@Lkabeer.com>
2018-10-09 16:23:11 +0200

    version X0.2

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
     return gulp.src('./scss/*.scss')
         .pipe(sass())
@@ -9,6 +10,14 @@ gulp.task('sass', function() {
         .pipe(browserSync.stream());
 });
 
+// Move the javascript files into our /js folder
+// gulp.task('js', function() {
+//     return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/popper.min.js'])
+//         .pipe(gulp.dest("./js"))
+//         .pipe(browserSync.stream());
+// });
+
+// Static Server + watching scss/html files
 gulp.task('serve', ['sass'], function() {
     
     browserSync.init({
